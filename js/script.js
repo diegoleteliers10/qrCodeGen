c255a7909bb9ad00e8ecaf843e540d08c6e6035c
Diego Letelier <diegoleteliers10@gmail.com>
2023-10-02 17:05:12 -0300

    agregando responsive

--- js/script.js
+++ js/script.js
@@ -24,11 +24,30 @@ document.querySelector('.butonQr').addEventListener('click',()=>{
       </div>
     </div>
     <div class="buttonsCont">
-      <button class="btnCopy">Download</button>
-      <button class="btnCopy">Share</button>
+      <button class="btnDownload">
+        Download
+        <svg xmlns="http://www.w3.org/2000/svg" width="15px" height="15px" fill="none" stroke-width="1.5" viewBox="0 0 24 24" color="#ffffff"><path stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" d="M6 20h12M12 4v12m0 0 3.5-3.5M12 16l-3.5-3.5"></path></svg>
+      </button>
+      <button class="btnCopy">
+        Share
+        <svg xmlns="http://www.w3.org/2000/svg" width="15px" height="15px" fill="none" stroke-width="1.5" viewBox="0 0 24 24" color="#ffffff"><path stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" d="M14 11.998C14 9.506 11.683 7 8.857 7H7.143C4.303 7 2 9.238 2 11.998c0 2.378 1.71 4.368 4 4.873a5.3 5.3 0 0 0 1.143.124"></path><path stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" d="M10 11.998c0 2.491 2.317 4.997 5.143 4.997h1.714c2.84 0 5.143-2.237 5.143-4.997 0-2.379-1.71-4.37-4-4.874A5.304 5.304 0 0 0 16.857 7"></path></svg>
+      </button>
     </div>`;
 
     document.querySelector('.logoQr').addEventListener('click', () => {
         window.location.reload();
     });
+
+    document.querySelector('.btnDownload').addEventListener('click', () => {
+        var link = document.createElement('a');
+        link.download = 'qr.png';
+        link.href = qr.toDataURL();
+        link.click();
+    })
+
+    document.querySelector('.btnCopy').addEventListener('click', ()=>{
+        var copyText = url;
+        navigator.clipboard.writeText(copyText);
+        alert('Copied the text: ' + copyText);
+    })
 })
